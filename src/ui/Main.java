50d860c51a13dd50a7f87202bc8b3c45aa9ab895
User <xducky@students.cs.ubc.ca>
2019-07-04 22:16:49 -0700

    Done

---
+++ src/ui/Main.java
@@ -0,0 +1,21 @@
+package ui;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        fn1();
+        fn2();
+        System.out.print(";");
+    }
+
+    public static void fn1()
+    {
+        System.out.print ("Hello ");
+    }
+
+    public static void fn2()
+    {
+        System.out.print ("World");
+    }
+}
